74fe9673d57208704cdbae364ace9529b747453f
onyangoouma <jacksonouma04@gmail.com>
2022-06-19 15:17:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return 'This one is on me!'
+  }
+  else if(feet > 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else{
+    return "No can do."
+  }
 }
+scuberGreetingForFeet(199)
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return (destination === "NYC"? 'Ok, sounds good.':'No go.');
 }
+ternaryCheckCity("NYC")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
